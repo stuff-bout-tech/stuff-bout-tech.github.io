c86977346bdc3c1db35f65362cf5f8ef4e84ecc0
stuff-bout-tech <techstuff@maildrop.cc>
2017-04-27 17:38:29 +0100

    Add files via upload

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,7 @@
+
+var tipuesearch = {"pages": [
+     {"title": "coming soon", "text": "If this is showing, it means mad things are happening", "tags": "coming soon", "url": "../index.html"},
+]};
+
+
+
